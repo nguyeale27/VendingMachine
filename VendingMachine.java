c3abdce183ead4d4c757688fa165a872bc8b28c2
nguyeale27 <nguyenalex927@gmail.com>
2021-03-11 10:50:28 -0500

    Payment method created

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,25 +1,44 @@
 
 import java.io.File;
+import java.io.FileReader;
 //import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
-
 class VendingMachine{
-    //Enum e = {'A' = 1, 'B', 'C','D'};
+    //Enum e = {'A' = 1, 'B', 'C','D'}; //rows would be easier to get with enum statement
     Scanner s = new Scanner(System.in);
-    
+    VendingSnack[][] inventory; //keeps track of items in VendingMachine
     public VendingMachine(){
-        File f = new File("Text.txt");
-
+        inventory = new VendingSnack[10][10];
+        //File f = new File("Text.txt");
+        //FileReader fr = new FileReader("input.json");
+        //fr.close();
     }
 public static void main (String[] args){
     VendingMachine vm = new VendingMachine();
+    vm.inventory[0][0] = new VendingSnack("Snickers", 10, 1.35);
+    vm.calculatePayment(0,0);
 }
-public int calculatePayment(){
+public void calculatePayment(int row, int column){
+double price = inventory[row][column].getPrice();
 
-return 0;
-}
-public void getPrice(){
+System.out.println("Selected " + inventory[row][column].getName() + ".");
+System.out.println("Total price is: " + price + ".");
 
+boolean hasPaid = false;
+while(hasPaid == false){
+
+    System.out.println("Please enter payment amount.");
+double payment = s.nextDouble();
+if(payment >= price){
+    double d = payment - price;
+    System.out.printf("Thank you. Your change is: %.2f", d);
+    hasPaid = true;
 }
+else{
+    System.out.println("Payment is less than price.");
+}
+}
+}
+
 }
--- VendingSnack.java
+++ VendingSnack.java
@@ -1,3 +1,19 @@
 class VendingSnack{
-    
+    private String name;
+    private int amount;
+    private double price;
+    public VendingSnack(String n, int a, double p){
+        name = n;
+        amount = a;
+        price = p;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAmount(){
+        return amount;
+    }
+    public double getPrice(){
+        return price;
+    }
 }
