821ff64ee687a009c714bd4c00852996a06cdfcb
nguyeale27 <nguyenalex927@gmail.com>
2021-03-11 09:46:03 -0500

    Test

---
+++ VendingMachine.java
@@ -0,0 +1,25 @@
+
+import java.io.File;
+//import java.util.Enumeration;
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+
+class VendingMachine{
+    //Enum e = {'A' = 1, 'B', 'C','D'};
+    Scanner s = new Scanner(System.in);
+    
+    public VendingMachine(){
+        File f = new File("Text.txt");
+
+    }
+public static void main (String[] args){
+    VendingMachine vm = new VendingMachine();
+}
+public int calculatePayment(){
+
+return 0;
+}
+public void getPrice(){
+
+}
+}
