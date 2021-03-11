70bdd9d71e4a672f18294fbe6805833cf8c99082
nguyeale27 <nguyenalex927@gmail.com>
2021-03-11 16:57:16 -0500

    Transactions now recorded into separate txt file

--- VendingMachine.java
+++ VendingMachine.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.logging.FileHandler;
+
 
 class VendingMachine{
      //rows would be easier to get with enum statement
@@ -14,25 +14,19 @@ class VendingMachine{
     VendingSnack[][] inventory; //keeps track of items in VendingMachine
     File file, input;
     FileWriter fw;
+    FileReader fr;
+    
     public VendingMachine(){
         inventory = new VendingSnack[10][10];
-        file = new File("Text.txt");
+        file = new File("Transactions.txt");
         input = new File("input.json");
-        try {
-            fw = new FileWriter(file);
-            fw.write("Testing");
-            fw.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        
         
     }
 public static void main (String[] args){
     VendingMachine vm = new VendingMachine();
-    System.out.println(vm.file.canWrite());
-    //vm.inventory[0][0] = new VendingSnack("Snickers", 10, 1.35);
-    //vm.calculatePayment(0,0);
+    vm.inventory[0][0] = new VendingSnack("Snickers", 10, 1.35);
+    vm.calculatePayment(0,0);
 }
 public void calculatePayment(int row, int column){
 double price = inventory[row][column].getPrice();
@@ -46,14 +40,27 @@ while(hasPaid == false){
     System.out.println("Please enter payment amount.");
 double payment = s.nextDouble();
 if(payment >= price){
-    double d = payment - price;
-    System.out.printf("Thank you. Your change is: %.2f", d);
+    double change = payment - price;
+    System.out.printf("Thank you. Your change is: %.2f", change);
     hasPaid = true;
+    recordTransaction(row, column, price, payment, change);
 }
 else{
     System.out.println("Payment is less than price.");
 }
 }
-}
 
+}
+private void recordTransaction(int row, int column, double price, double payment, double change){
+    try {
+        fw = new FileWriter(file);
+        String name = inventory[row][column].getName();
+        String s = String.format("Transaction: %s purchased. Payment: $%.2f. Total Change: $%.2f.", name, payment, change);
+        fw.write(s);
+        fw.close();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+}
 }
