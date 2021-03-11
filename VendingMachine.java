9b315605fe30228fc5e1dfa42761b07b40de9bd5
nguyeale27 <nguyenalex927@gmail.com>
2021-03-11 16:07:52 -0500

    Able to write to text for transactions

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,23 +1,38 @@
 
 import java.io.File;
 import java.io.FileReader;
-//import java.util.Enumeration;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
+
 class VendingMachine{
-    //Enum e = {'A' = 1, 'B', 'C','D'}; //rows would be easier to get with enum statement
+     //rows would be easier to get with enum statement
+     HashMap<Character,Integer> hm = new HashMap<Character, Integer>();
     Scanner s = new Scanner(System.in);
     VendingSnack[][] inventory; //keeps track of items in VendingMachine
+    File file, input;
+    FileWriter fw;
     public VendingMachine(){
         inventory = new VendingSnack[10][10];
-        //File f = new File("Text.txt");
-        //FileReader fr = new FileReader("input.json");
-        //fr.close();
+        file = new File("Text.txt");
+        input = new File("input.json");
+        try {
+            fw = new FileWriter(file);
+            fw.write("Testing");
+            fw.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 public static void main (String[] args){
     VendingMachine vm = new VendingMachine();
-    vm.inventory[0][0] = new VendingSnack("Snickers", 10, 1.35);
-    vm.calculatePayment(0,0);
+    System.out.println(vm.file.canWrite());
+    //vm.inventory[0][0] = new VendingSnack("Snickers", 10, 1.35);
+    //vm.calculatePayment(0,0);
 }
 public void calculatePayment(int row, int column){
 double price = inventory[row][column].getPrice();
